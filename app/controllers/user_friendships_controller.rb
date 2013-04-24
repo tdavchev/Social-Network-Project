5015db2c41c1a2e7007088762c1ed24515f5bef4
Todor Davchev <todor.davchev@gmail.com>
2013-04-23 20:13:05 -0400

    Now this is what I call add friend yes/ no

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -13,4 +13,20 @@ class UserFriendshipsController < ApplicationController
         rescue ActiveRecord::RecordNotFound
             render file: 'public/404', status: :not_found
     end
+    
+    def create
+        if params[:friend_id]
+          @friend = User.where(profile_name: params[:friend_id]).first
+          @user_friendship = current_user.user_friendships.new(friend: @friend)
+          if @user_friendship.save
+            flash[:success] = "Friendship created."
+          else
+            flash[:error] = "There was a problem."
+          end
+          redirect_to profile_path(@friend)
+        else
+          flash[:error] = "Friend required"
+          redirect_to root_path
+        end
+    end
 end
--- test/functional/user_friendships_controller_test.rb
+++ test/functional/user_friendships_controller_test.rb
@@ -1,54 +1,106 @@
 require 'test_helper'
 
 class UserFriendshipsControllerTest < ActionController::TestCase
-    context "#new" do
-        context "when not logged in" do
-            should "redirect to the login page" do
-                get :new
-                assert_response :redirect
-            end
-        end
-        
-        context "when logged in" do
-            setup do
-                sign_in users(:jason)
-            end
-            
-            should "get new and return success" do
-                get :new
-                assert_response :success
-            end
-            
-            should "set a flash error if the friend_id params is missing" do
-                get :new, {}
-                assert_equal "Friend required", flash[:error]
-            end
-            
-            should "display the friend's name" do
-                get :new, friend_id: users(:jim)
-                assert_match /#{users(:jim).full_name}/, response.body
-            end
-            
-            should "assign a new user friendship to the correct friend" do
-                get :new, friend_id: users(:jim)
-                assert_equal users(:jim), assigns(:user_friendship).friend
-            end
-            
-            should "assign a new user friendship to the currently logged in user" do
-                get :new, friend_id: users(:jim)
-                assert_equal users(:jason), assigns(:user_friendship).user
-            end
-            
-            should "return a 404 if no friend was found" do 
-                get :new, friend_id: 'invalid'
-                assert_response :not_found
-            end
-            
-            #should "ask if you really want to friend the user" do 
-             #   get :new, freind_id: users(:jim)
-              #  assert_match /Do you really want to friend #{users(:jim).full_name}?/, response.body
-            #end
-
-        end
+  context "#new" do
+    context "when not logged in" do
+      should "redirect to the login page" do
+        get :new
+        assert_response :redirect
+        assert_redirected_to login_path
+      end
     end
+
+    context "when logged in" do
+      setup do
+        sign_in users(:jason)
+      end
+
+      should "get new without error" do
+        get :new
+        assert_response :success
+      end
+
+      should "should set a flash error if the friend_id param is missing" do
+        get :new, {}
+        assert_equal "Friend required", flash[:error]
+      end
+
+      should "display a 404 page if no friend is found" do
+        get :new, friend_id: 'invalid'
+        assert_response :not_found
+      end
+
+      should "display the friend's name" do
+        get :new, friend_id: users(:jim)
+        assert_match /#{users(:jim).full_name}/, response.body
+      end
+
+      should "assign a user friendship" do
+        get :new, friend_id: users(:jim)
+        assert assigns(:user_friendship)
+      end
+
+      should "assign a user friendship with the user as current user" do
+        get :new, friend_id: users(:jim)
+        assert_equal assigns(:user_friendship).user, users(:jason)
+      end
+
+      should "assign a user friendship with the correct friend" do
+        get :new, friend_id: users(:jim)
+        assert_equal assigns(:user_friendship).friend, users(:jim)
+      end
+    end
+  end
+  
+  context "#create" do
+    context "when not logged in" do
+      should "redirect to the login page" do
+        get :new
+        assert_response :redirect
+        assert_redirected_to login_path
+      end
+    end
+
+    context "when logged in" do
+      setup do
+        sign_in users(:jason)
+      end
+
+      context "with no friend_id" do
+        setup do
+          post :create
+        end
+
+        should "set the flash error message" do
+          assert !flash[:error].empty?
+        end
+
+        should "set redirect to root" do
+          assert_redirected_to root_path
+        end
+      end
+
+      context "with a valid friend_id" do
+        setup do
+          post :create, friend_id: users(:mike)
+        end
+
+        should "assign a friend object" do
+          assert_equal users(:mike), assigns(:friend)
+        end
+
+        should "assign a user_friendship object" do
+          assert assigns(:user_friendship)
+          assert_equal users(:jason), assigns(:user_friendship).user
+          assert_equal users(:mike), assigns(:user_friendship).friend
+        end
+
+        should "create a user friendship" do
+          assert users(:jason).friends.include?(users(:mike))
+        end
+      end
+
+
+    end
+  end
 end
