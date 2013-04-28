a171d523ff539b863d0bfd456f05b24add2daad5
todor <t.davchev@gmail.com>
2013-04-28 21:34:58 +0100

    Simplified edit. Added Decorator

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -54,7 +54,7 @@ class UserFriendshipsController < ApplicationController
     end
 
     def edit
-      @user_friendship = current_user.user_friendships.find(params[:id])
+      @user_friendship = current_user.user_friendships.find(params[:id]).decorate
       @friend = @user_friendship.friend
     end
 end
